2fd7e5e0bc24260cf46f1e108e7c191d2d8fcd0d
Collins Kibet <kollcibe05@gmail.com>
2026-03-10 20:15:05 +0300

    revert

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 
-// import jsonServer from 'json-server'; //Yeah, the only diff!
+import jsonServer from 'json-server'; //Yeah, the only diff!
 
-// const server = jsonServer.create();
-// const router = jsonServer.router('pokemon.json'); 
-// const middlewares = jsonServer.defaults();
+const server = jsonServer.create();
+const router = jsonServer.router('pokemon.json'); 
+const middlewares = jsonServer.defaults();
 
-// // Get the port from Render's environment variable (or use 3000 locally)
-// const port = process.env.PORT || 3000;
+// Get the port from Render's environment variable (or use 3000 locally)
+const port = process.env.PORT || 3000;
 
-// server.use(middlewares);
-// server.use(router);
+server.use(middlewares);
+server.use(router);
 
-// server.listen(port, () => {
-//   console.log(`JSON Server is running in ESM mode on port ${port}`);
-// });
+server.listen(port, () => {
+  console.log(`JSON Server is running in ESM mode on port ${port}`);
+});
 
 
-import { create, router as _router, defaults } from 'json-server'
+// import { create, router as _router, defaults } from 'json-server'
 
-const server = create()
-const router = _router('db.json') // path to your data file
-const middlewares = defaults()
+// const server = create()
+// const router = _router('db.json') // path to your data file
+// const middlewares = defaults()
 
-server.use(middlewares)
-server.use(router)
+// server.use(middlewares)
+// server.use(router)
 
-const PORT = process.env.PORT || 3000
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`)
-})
+// const PORT = process.env.PORT || 3000
+// server.listen(PORT, () => {
+//   console.log(`JSON Server is running on port ${PORT}`)
+// })
