e21af21727a7254bdb03bea8ef5a65d8a93b1ff5
Collins Kibet <kollcibe05@gmail.com>
2026-03-10 18:15:39 +0300

    CommonJS

---
+++ index.js
@@ -0,0 +1,14 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('pokemon.json'); // Path to your data file
+const middlewares = jsonServer.defaults();
+
+// Get the port from Render's environment variable (or use 3000 locally)
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => {
+  console.log(`JSON Server v0.17.4 is running successfully on port ${port}`);
+});
